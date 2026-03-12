1e863f0386ed4b79d523c3235a632a1b85f73262
Rehann <rehanngreens@gmail.com>
2026-03-12 08:07:11 +0530

    updated main

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print()
+print()from network_simulation.topology import start_network
+from ai_detection.anomaly_detector import detect_anomaly
+from automation.auto_block import block_attacker
+
+def main():
+    print("Starting network simulation...")
+    start_network()
+
+    print("Monitoring traffic...")
+    anomaly = detect_anomaly()
+
+    if anomaly:
+        print("Threat detected!")
+        block_attacker()
+    
+if __name__ == "__main__":
+    main()
